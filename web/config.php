699713d21bed1c128bb1a9aff9706db2293ea38b
zds2 <71508695+zds2@users.noreply.github.com>
2020-09-26 20:27:13 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY")); //这里改成您的api key
+define('APIKEY', 'AIzaSyAM5OSWfPiRlZ2E-AcV5M2NBtRboUaQ1H8'); //这里改成您的api key
 define('GJ_CODE', 'TW');//国家代码
 define('SITE_NAME', 'Youtube代理');//网站名字
 define('TITLENAME', 'Youtube代理');//网站名字
 define('EN2DEKEY', 'gfw-breaker');//一个随机字符串，加解密KEY
-define('EMAIL', 'lixiaoming0909@gmail.com');//邮箱
+define('EMAIL', 'admin@zdszds.tk');//邮箱
 define('PLAYLIST_ID', getenv("PLAYLIST_ID"));//推荐内容播放列表
 ?>
